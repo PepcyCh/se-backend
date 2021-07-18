c66547ae403277e33b72133b710e11d95ba83d22
PepcyCh <pepsich86@163.com>
2021-07-18 09:42:34 +0800

    remove useless service

--- src/main.rs
+++ src/main.rs
@@ -16,36 +16,6 @@ use diesel::{r2d2::ConnectionManager, MysqlConnection};
 
 type DbPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 
-#[get("/")]
-async fn user_index() -> impl Responder {
-    NamedFile::open("user/index.html")
-}
-
-#[get("/{filename}")]
-async fn user_default_get(web::Path(filename): web::Path<String>) -> impl Responder {
-    NamedFile::open(format!("user/{}", filename))
-}
-
-#[get("/")]
-async fn doctor_index() -> impl Responder {
-    NamedFile::open("doctor/index.html")
-}
-
-#[get("/{filename}")]
-async fn doctor_default_get(web::Path(filename): web::Path<String>) -> impl Responder {
-    NamedFile::open(format!("doctor/{}", filename))
-}
-
-#[get("/")]
-async fn admin_index() -> impl Responder {
-    NamedFile::open("admin/index.html")
-}
-
-#[get("/{filename}")]
-async fn admin_default_get(web::Path(filename): web::Path<String>) -> impl Responder {
-    NamedFile::open(format!("admin/{}", filename))
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
@@ -64,22 +34,16 @@ async fn main() -> std::io::Result<()> {
             // user
             .service(
                 web::scope("/user")
-                    .service(user_index)
-                    .service(user_default_get)
                     .configure(user::config),
             )
             // doctor
             .service(
                 web::scope("/doctor")
-                    .service(doctor_index)
-                    .service(doctor_default_get)
                     .configure(doctor::config),
             )
             // administrator
             .service(
                 web::scope("/admin")
-                    .service(admin_index)
-                    .service(admin_default_get)
                     .configure(admin::config),
             )
     })
